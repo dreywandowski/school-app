d1af7036fa45849f977d7f5e4881174b34e88dac
dreywandowski <aduramimo@gmail.com>
2020-09-02 17:36:37 +0000

    'first'

---
+++ database/migrations/2020_09_02_150641_create_fees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+             $table->string('user');
+              $table->string('email');
+               $table->integer('amount');
+                $table->string('payment_ref');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fees');
+    }
+}
