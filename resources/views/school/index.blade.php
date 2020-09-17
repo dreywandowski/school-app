95536d6759dee863fef9a0d2dd0e2cbb4aa27fd4
dreywandowski <aduramimo@gmail.com>
2020-09-17 17:52:19 +0000

    styled home page

--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -6,39 +6,66 @@
 
         <title>SCHOOL APP</title>
 
-        <!-- Fonts -->
+         <!--Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300&display=swap" rel="stylesheet">
-        <link href="/css/index.css" rel="stylesheet">
+         <link rel="stylesheet" href="/css/index.css">
     </head>
 
     <body>
     
-    <div class="topnav">
-  <a class="active" href="#home">HOME</a>
-   <a href="#about">ABOUT</a>
-  <a href="#contact">CONTACT</a>
- 
-</div>
+    <div class="container">
+        <header>
+        <div class="header-logo">
+            <img class="big-img" src="/img/drey4.JPG">
+            <img class="small-img" src="/img/drey44.jpg">
+        </div>
+            
+       
 
-        <div class="first-content"><p><center><b> <h1>SCHOOLIFY</h1></b>
-     
-    Our app automates the processess involved in school.<br>
+        <div class="navigation">
+            <nav class="nav-bar">
+            <ul>
+                <li><a href="/school" class="active">HOME</a></li>
+                <li><a href="#">CURRICULUM</a></li>
+                <li><a href="/login">LOGIN</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+            </nav>
+        </div>
+        </header>
+
+
+ 
+        <div class="mainContent">
+            <div class="contentArticle">
+                <article class="firstArticle">
+                    <header><h3>THE SCHOOL APP</h3></header>
+                    <content>
+                       Our app automates the processess involved in school.<br>
 Accept payments, upload course material, take/grade exams, etc.<br>
 Its a full package! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  Mauris mi dui, ornare nec accumsan eu, viverra vel neque. Nullam congue lorem risus, nec vehicula turpis posuere ac. 
-Aliquam feugiat nulla ut est vehicula pharetra. Praesent ac tincidunt dui, non pulvinar sapien. Quisque fringilla mi nec egestas euismod</b></center></p></div><br/><br>
-</center></p>
+Aliquam feugiat nulla ut est vehicula pharetra. Praesent ac tincidunt dui, non pulvinar sapien. Quisque fringilla mi nec egestas euismod
+                    </content>
+                </article>
 
-<a href="/login">Login</a> | 
-<a href="/register">Register</a>
+                <img class="hi-mage-1" src="/img/5836.jpg">
+                
+                <form name="start" class="start" action="/register">
+                    <a href="/register"><button type="submit" class="get-started"><span>Get started</span></button></a>
+                </form>
 
+            </div>
 
-<div class="second-content">
-    <img id='img' src="/img/stud.jpg">
-</div>
+            <div class="hi-mage">
+                <img class="hi-mage-2" src="/img/5836.jpg">
+            </div>
+        </div>
+        <footer class="footer">
+            <p>Copyright &COPY; ADURAMIMO, 2020.</p>
+        </footer>
+    </div>
+</body>
 
+    </html>    
 
-
-    
-    </body>
-    </html>
