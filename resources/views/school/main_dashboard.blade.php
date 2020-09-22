5e3b82ccd15cab6427dae51b65fd6ef29fb023dc
dreywandowski <aduramimo@gmail.com>
2020-09-22 18:25:06 +0000

    defined layouts

---
+++ resources/views/school/main_dashboard.blade.php
@@ -0,0 +1,20 @@
+ @include('layout.header')
+<body>
+
+   <div id="main" class="row">
+           @yield('content')
+   </div>
+
+   <p>
+
+    Hi
+    
+   </p>
+   <footer class="row">
+       @include('layout.footer')
+   </footer>
+</div>
+</body>
+</html>
+
+
