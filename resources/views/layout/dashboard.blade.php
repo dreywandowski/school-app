23c2365b5ae5549bc5cbc491e4145f5bca04b327
dreywandowski <aduramimo@gmail.com>
2020-09-30 17:29:00 +0000

    student dashboard

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href='css/app.css' rel="stylesheet">
 
     <link rel="stylesheet" href="/css/dashboard.css">
     <!-- Right Side Of Navbar -->
